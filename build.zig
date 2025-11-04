6b663401241e74acbad42ed84baf0e3c300f29de
C0d3-5t3w <stewy1994@gmail.com>
2025-11-04 10:17:03 -0800

    init

---
+++ build.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const root_module = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "mTerm",
+        .root_module = root_module,
+    });
+
+    exe.addCSourceFile(.{
+        .file = b.path("src/main.m"),
+        .flags = &[_][]const u8{},
+    });
+
+    exe.linkLibC();
+
+    exe.linkFramework("Cocoa");
+    exe.linkFramework("Metal");
+    exe.linkFramework("MetalKit");
+    exe.linkFramework("QuartzCore");
+    exe.linkFramework("CoreGraphics");
+    exe.linkFramework("Foundation");
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
---
+++ src/main.m
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#import <Cocoa/Cocoa.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <Foundation/Foundation.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import <QuartzCore/CAMetalLayer.h>
+#import <simd/simd.h>
+
+int main() {}
